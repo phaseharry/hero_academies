8170e483db5080c82322efebd567af868d304500
Harry C <harrychenhc@icloud.com>
2018-09-23 00:01:33 -0400

    updated routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,16 +2,56 @@ const express = require('express');
 const { School, Student } = require('../db').models;
 const router = express.Router();
 
+router.get('/schools/:id', (req, res, next) => {
+  School.findById(req.params.id, {
+    include: [{ model: Student }],
+  })
+    .then(school => res.json(school))
+    .catch(error => next(error));
+});
+
 router.get('/schools', (req, res, next) => {
   School.findAll()
     .then(schools => res.json(schools))
-    .catch(next);
+    .catch(error => next(error));
+});
+
+router.post('/schools', (req, res, next) => {
+  School.create(req.body)
+    .then(school => res.json(school))
+    .catch(error => next(error));
+});
+
+router.delete('/schools/:id', (req, res, next) => {
+  School.findById(req.params.id)
+    .then(school => school.destroy())
+    .then(() => res.sendStatus(204))
+    .catch(error => next(error));
+});
+
+router.get('/students/:id', (req, res, next) => {
+  Student.findById(req.params.id)
+    .then(student => res.json(student))
+    .catch(error => next(error));
 });
 
 router.get('/students', (req, res, next) => {
   Student.findAll()
     .then(students => res.json(students))
-    .catch(next);
+    .catch(error => next(error));
+});
+
+router.post('/students', (req, res, next) => {
+  Student.create(req.body)
+    .then(student => res.json(student))
+    .catch(error => next(error));
+});
+
+router.delete('/students/:id', (req, res, next) => {
+  Student.findById(req.params.id)
+    .then(student => student.destroy())
+    .then(() => res.sendStatus(204))
+    .catch(error => next(error));
 });
 
 module.exports = router;
